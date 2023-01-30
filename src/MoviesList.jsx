c80b9cc0b4a10e5755dabbec1f3ad48560a692e1
Nicolas Germeau <ngermeau@icloud.com>
2023-01-30 12:19:35 +0100

    working barbone version of infinite scroll

--- src/MoviesList.jsx
+++ src/MoviesList.jsx
@@ -2,41 +2,43 @@ import Movie from "./Movie"
 import { Fragment, useEffect, useRef, useState } from "react"
 
 
-// 1) render 
-// 2) useEffect
-// 3) rerender if state change
+/**
+ * 1) render 
+ * 2) new element ref (last div) to be observed is put into a ref and our observer observe it
+ * 3) useEffect is triggered initialy after the first render and data is loaded 
+ * 4) data loading is updating movie state with new movies -> render will be triggered 
+ * 5) unobserving the current observed element  
+ ----
+ * 5) render with new movies  
+ * 6) new element ref (last div) to be observed is put into a ref and our observer observe it
+ * 7) if element is displayed on screen with treshold, observer changing the state of query to fetch next batch  
+ * 8)   because query state has changed -> 1) useeffect will be trigger (watching on query) and a render will be triggered
+ * 9)   useEffect is triggered and data is loaded 
+ * 10)  data loading is updating movie state with new movies -> render will be triggered 
+ * 11)  unobserving the current observed element
+ */
 
 const MoviesList = (props) => {
 
+  const take = 4
   const [movies, setMovies] = useState([])
-  const [offset, setOffset] = useState({ offset: 0 })
+  const [query, setQuery] = useState({ skip: 0, take: take })
   const observed = useRef()
 
-
   const observer = useRef(new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) {
-      console.log("last element being seen loading changing the state")
-      setOffset((prevOffset) => {
-        return { offset: 2 + prevOffset.offset }
-      })
-    }
+    if (entries[0].isIntersecting) { setQuery(prevQuery => ({ skip: prevQuery.skip + take, take: take })) }
   }, { threshold: 0.1 }))
 
   useEffect(() => {
-    console.log('in useeffect')
-    requestMovies(offset, 2)
+    requestMovies(query)
     observer.current.unobserve(observed.current)
-  }, [offset]) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [query]) // eslint-disable-line react-hooks/exhaustive-deps
 
 
-  async function requestMovies(offset, limit) {
-    console.log("loading data")
-    const res = await fetch(`https://wbyd-production.up.railway.app/movie?offset=${offset.offset}&limit=2`)
+  async function requestMovies(query) {
+    const res = await fetch(`https://wbyd-production.up.railway.app/movie?offset=${query.skip}&limit=${query.take}`)
     const moviesJson = await res.json()
-    console.log(movies)
-    console.log(moviesJson)
     setMovies([...movies, ...moviesJson.data])
-    // observer.current.unobserve(observed.current)
   }
 
   const setObserved = (element) => {
